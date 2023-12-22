6a35da64d11ef934e733d593f3b9ffadeef8d2ec
Digastrick <110335028+Digastrick@users.noreply.github.com>
2023-12-22 16:51:09 +0300

    Add files via upload

---
+++ src/stores/PostModule.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+
+export const PostModule = {
+    state:() =>({
+    posts:[]
+  }),
+  mutations:{
+    SET_POSTS(state,posts){
+      state.posts = posts;
+    },
+    RESET_POSTS(state) {
+    state.posts = []
+  }
+  },
+  actions:{
+    fetchPosts({commit},id){
+      return axios.get(`http://localhost:5173/posts?userId=${id}`)
+      .then(posts => {
+        commit('SET_POSTS',posts.data)
+      })
+      .catch(error => {
+        console.error('Failed to fetch posts:', error)
+      })
+    },
+    resetPosts({ commit }) {
+    commit('RESET_POSTS')
+  }
+  }
+}
+</script>
---
+++ src/stores/UsersModule.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+
+export const UsersModule = {
+    state:() =>({
+        users: [],
+  }),
+  mutations:{
+    SET_USERS(state, users) {
+      state.users = users;
+    },
+    RESET_USERS(state) {
+    state.users = []
+  }
+  },
+  actions:{
+    fetchUsers({ commit }) {
+      return axios.get('http://localhost:5173/users')
+      .then(users => {
+        commit('SET_USERS',users.data);
+      })
+        .catch(error => {
+          console.error('Failed to fetch users:', error);
+        });
+    },
+    resetUsers({ commit }) {
+    commit('RESET_USERS')
+  }
+  }
+}
+</script>
